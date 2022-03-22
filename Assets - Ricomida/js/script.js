aeb4bd221f02e352b844317b2fd5ff597277b96f
jovejar <100956893+jovejar@users.noreply.github.com>
2022-03-23 01:04:21 +0300

    Initial commit

---
+++ Assets - Ricomida/js/script.js
@@ -0,0 +1,25 @@
+          $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+          });
+
+          $(function(){
+
+            $("a").click(function(event){
+              if (this.hash !== "") {
+                event.preventDefault();
+          
+                var gato = this.hash;
+          
+                $("html, body").animate({
+                  scrollTop: $(gato).offset().top
+                }, 800, function(){
+                  window.location.hash = gato;
+                });
+              }
+            });
+          
+            $(function () {
+              $('[data-toggle="tooltip"]').tooltip()
+            })
+          
+          });
